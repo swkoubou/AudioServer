fc7d2657a595ef5a7ca670f76ea18288d010874d
snakazawa <nakazawa.tms0314@gmail.com>
2014-06-05 15:39:03 +0900

    AlertViewModelとLoadingViewModelにjsdoc追加

--- js/ViewModel/LoadingViewModel.js
+++ js/ViewModel/LoadingViewModel.js
@@ -5,11 +5,28 @@ $(function () {
 
     var ns = util.namespace("swkoubou.audioserver.viewmodel");
 
-    ns.LoadingViewModel = function () {
+    /**
+     * ローディング状態のViewModelコンストラクタ関数
+     *
+     * @constructor
+     */
+    ns.LoadingViewModel = function LoadingViewModel() {
         var that = this;
 
+        /**
+         * ローディング状態かどうか
+         *
+         * @type {boolean}
+         */
         that.isLoading = ko.observable(false);
 
+        /**
+         * Deferredインターフェースを備えた関数に対し、Loadingの状態変更を前後に挿入しラッピングする
+         * 対象の関数が実行される前に isLoading が true になり、対象のの処理が終了したら isLoading が false になる
+         *
+         * @param {function(...): Deferred} deferred Deferredインターフェースを備えているラッピング対象の関数
+         * @returns {function(...): Deferred} Deferredインターフェースを備えたラッピング済み関数
+         */
         that.wrapDeferred = function (deferred) {
             if (!_.isFunction(deferred)) {
                 return deferred;
@@ -24,6 +41,12 @@ $(function () {
             };
         };
 
+        /**
+         * object の持つ複数の関数に対してLoadingラッピングを適用する（objectに対して破壊的）
+         *
+         * @param {!Object} object 対象のオブジェクト
+         * @param {Array.<string>} methodNames 対象のオブジェクトが持つ、Loadingラッピングの対象メソッド名群
+         */
         that.wrapDeferredAll = function (object, methodNames) {
             _.each(methodNames, function (methodName) {
                 if (_.isFunction(object[methodName])) {
