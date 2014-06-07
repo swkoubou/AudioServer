5d97767beac527ad11f5852a22a8f2aa08c2398c
sunya <mugen.Xyz@gmail.com>
2014-06-07 14:48:08 +0900

    add variables and fixed query.
    変数の追加とクエリの変更(途中)

--- api/uploadtest.php
+++ api/uploadtest.php
@@ -4,12 +4,13 @@
 <title>sample</title>
 </head>
 <body>
-<form action="status.php" method="post" enctype="multipart/form-data">
-  mid：<br>
-  <input type="text" name="type" /><br>
-  pid：<br>
-  <input type="text" name="value" /><br>
-  <input type="submit" value="アップロード" />
-</form>
+<h4 class="modal-title" id="myModalLabel">Upload Music</h4>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+
+                <label class="control-label">User name</label>
+                <input type="file" name="file" multiple="multiple" data-bind="">
+            <button type="submit" class="btn btn-primary col-xs-5 pull-right"
+                    data-bind="css: { disabled: !validationUserName() }">Upload</button>
+    </form>
 </body>
 </html>
